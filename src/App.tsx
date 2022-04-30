aa46ed4654096dba6cc00f4c8473bd97ed04560c
Enzo Do rosario <enzo.dorosario@redlab.io>
2022-04-30 22:14:41 +0200

    feat: use material component in basic app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Button from "@mui/material/Button";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return <Button variant="contained">Hello Redlab!</Button>;
 }
 
 export default App;
