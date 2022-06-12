37246dd5f468aa58a42297da7e67a1a4920cb827
Enzo Do rosario <enzo.dorosario@redlab.io>
2022-06-12 17:57:01 +0200

    feat: add basic routing

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+function Home() {
+  return (
+    <Button variant="contained" color="error">
+      Hello Redlab
+    </Button>
+  );
+}
+
+export default Home;
